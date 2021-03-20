856f1299756edbb947eb29c1c9ba8b0e856ad4f9
Tim van Dijen <tim.dijen@minbzk.nl>
2021-03-20 10:35:10 +0100

    Create SerializableXMLTest.php

---
+++ tests/SerializableXMLTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XML;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\AbstractSerializableXML;
+
+/**
+ * Test for AbstractSerializableXML classes to perform default serialization tests.
+ *
+ * @runTestsInSeparateProcesses
+ * @package simplesamlphp\xml-common
+ */
+abstract class SerializableXMLTest extends TestCase
+{
+    /** @var class-string */
+    protected static string $element;
+
+    /** @var \DOMDocument */
+    protected static DOMDocument $xmlDocument;
+
+    /**
+     * Test serialization / unserialization.
+     */
+    public function testSerialization(): void
+    {
+        $element = static::$element;
+        $document = static::$xmlDocument;
+
+        if ($element === null || !class_exists($element)) {
+            $this->markTestSkipped('Unable to run ' . static::class . '::testSerialization(). Please set ' . static::class . ':$element to a class-string representing the XML-class being tested');
+        } elseif ($document === null) {
+            $this->markTestSkipped('Unable to run ' . static::class . '::testSerialization(). Please set ' . static::class . ':$xmlDocument to a DOMDocument representing the XML-class being tested');
+        } else {
+            $this->assertEquals(
+                $document->saveXML($document->documentElement),
+                strval(unserialize(serialize($element::fromXML($document->documentElement))))
+            );
+        }
+    }
+}
