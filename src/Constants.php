daed5738bc22067c9b0abec2d2b877dc8a1d7647
Tim van Dijen <tvdijen@gmail.com>
2024-11-06 10:36:15 +0100

    Add XPath constants

--- src/Constants.php
+++ src/Constants.php
@@ -30,4 +30,24 @@ class Constants
      * The maximum amount of child nodes this library is willing to handle.
      */
     public const UNBOUNDED_LIMIT = 10000;
+
+    /**
+     * The namespace for the XML Path Language 1.0
+     */
+    public const XPATH10_URI = 'http://www.w3.org/TR/1999/REC-xpath-19991116';
+
+    /**
+     * The namespace for the XML Path Language 2.0
+     */
+    public const XPATH20_URI = 'http://www.w3.org/TR/2010/REC-xpath20-20101214/';
+
+    /**
+     * The namespace for the XML Path Language 3.0
+     */
+    public const XPATH30_URI = 'https://www.w3.org/TR/2014/REC-xpath-30-20140408/';
+
+    /**
+     * The namespace for the XML Path Language 3.1
+     */
+    public const XPATH31_URI = 'https://www.w3.org/TR/2017/REC-xpath-31-20170321/';
 }
