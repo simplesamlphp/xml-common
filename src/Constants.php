b289b53c2585b52c7f50a6ffcb59379e55b2342e
Tim van Dijen <tvdijen@gmail.com>
2024-11-16 17:13:41 +0100

    Add comment on node-limit

--- src/Constants.php
+++ src/Constants.php
@@ -28,6 +28,7 @@ class Constants
 
     /**
      * The maximum amount of child nodes this library is willing to handle.
+     * By specification, this limit is 150K, but that opens up for denial of service.
      */
     public const UNBOUNDED_LIMIT = 10000;
 
