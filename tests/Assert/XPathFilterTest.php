03455400497f4295a9a05fde301b7f30e6a2dbd7
Tim van Dijen <tvdijen@gmail.com>
2024-11-26 17:33:31 +0100

    Remove unused use-statements

--- tests/Assert/XPathFilterTest.php
+++ tests/Assert/XPathFilterTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace SimpleSAML\XML\Test\Assert;
 
-use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
