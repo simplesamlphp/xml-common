37b72283d3245081945ad9f389d8d8d67e21ee45
Tim van Dijen <tvdijen@gmail.com>
2024-12-14 13:00:57 +0100

    Re-use new functionality in test-trait

--- src/TestUtils/SchemaValidationTestTrait.php
+++ src/TestUtils/SchemaValidationTestTrait.php
@@ -5,19 +5,10 @@ declare(strict_types=1);
 namespace SimpleSAML\XML\TestUtils;
 
 use DOMDocument;
-use Exception;
-use LibXMLError; // Officially spelled with a lower-case `l`, but that breaks composer-require-checker
 use PHPUnit\Framework\Attributes\Depends;
-use SimpleSAML\Assert\Assert;
-use SimpleSAML\XML\Exception\SchemaViolationException;
-use XMLReader;
+use SimpleSAML\XML\DOMDocumentFactory;
 
-use function array_unique;
 use function class_exists;
-use function implode;
-use function libxml_get_last_error;
-use function libxml_use_internal_errors;
-use function trim;
 
 /**
  * Test for AbstractElement classes to perform schema validation tests.
@@ -58,58 +49,20 @@ trait SchemaValidationTestTrait
                 . ':$xmlRepresentation to a DOMDocument representing the XML-class being tested',
             );
         } else {
-            $predoc = XMLReader::XML(self::$xmlRepresentation->saveXML());
-            Assert::notFalse($predoc);
+            // Validate before serialization
+            DOMDocumentFactory::schemaValidation(self::$xmlRepresentation->saveXML(), self::$schemaFile);
 
-            $pre = $this->validateDocument($predoc);
-            $this->assertTrue($pre);
-
+            // Perform serialization
             $class = self::$testedClass::fromXML(self::$xmlRepresentation->documentElement);
             $serializedClass = $class->toXML();
 
-            $postdoc = XMLReader::XML($serializedClass->ownerDocument->saveXML());
-            Assert::notFalse($postdoc);
-            $post = $this->validateDocument($postdoc);
-            $this->assertTrue($post);
+            // Validate after serialization
+            DOMDocumentFactory::schemaValidation($serializedClass->ownerDocument->saveXML(), self::$schemaFile);
+
+            // If we got this far and no exceptions were thrown, consider this test passed!
+            $this->addToAssertionCount(1);
         }
     }
-
-
-    /**
-     * @param \XMLReader $doc
-     * @return boolean
-     */
-    private function validateDocument(XMLReader $xmlReader): bool
-    {
-        libxml_use_internal_errors(true);
-
-        try {
-            $xmlReader->setSchema(self::$schemaFile);
-        } catch (Exception) {
-            $err = libxml_get_last_error();
-            throw new SchemaViolationException(trim($err->message) . ' on line ' . $err->line);
-        }
-
-        $msgs = [];
-        while ($xmlReader->read()) {
-            if (!$xmlReader->isValid()) {
-                $err = libxml_get_last_error();
-                if ($err instanceof LibXMLError) {
-                    $msgs[] = trim($err->message) . ' on line ' . $err->line;
-                }
-            }
-        }
-
-        if ($msgs) {
-            throw new SchemaViolationException(sprintf(
-                "XML schema validation errors:\n - %s",
-                implode("\n - ", array_unique($msgs)),
-            ));
-        }
-
-        return true;
-    }
-
 
     abstract public function testSerialization(): void;
 }
