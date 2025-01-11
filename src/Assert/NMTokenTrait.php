cd705cc96f25125f565c28d670b652ddde096814
Tim van Dijen <tvdijen@gmail.com>
2025-01-11 16:55:51 +0100

    Create assertion and type-class for xs:NMTOKEN

---
+++ src/Assert/NMTokenTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Assert;
+
+use InvalidArgumentException;
+
+/**
+ * @package simplesamlphp/xml-common
+ */
+trait NMTokenTrait
+{
+    /** @var string */
+    private static string $nmtoken_regex = '/^[\w.:-]+$/Du';
+
+
+    /**
+     * @param string $value
+     * @param string $message
+     */
+    protected static function validNMToken(string $value, string $message = ''): void
+    {
+        Assert::regex(
+            $value,
+            self::$nmtoken_regex,
+            $message ?: '%s is not a valid xs:NMTOKEN',
+            InvalidArgumentException::class,
+        );
+    }
+}
