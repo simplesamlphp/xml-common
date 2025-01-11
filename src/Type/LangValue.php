a11df6afdade11e1ff758cfb296fb63af9f92a06
Tim van Dijen <tvdijen@gmail.com>
2025-01-11 17:04:31 +0100

    Create assertion and type-class for xs:language

---
+++ src/Type/LangValue.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Type;
+
+use SimpleSAML\XML\Assert\Assert;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+/**
+ * @package simplesaml/xml-common
+ */
+class LangValue extends TokenValue
+{
+    /**
+     * Validate the value.
+     *
+     * @param string $value
+     * @throws \SimpleSAML\XML\Exception\SchemaViolationException on failure
+     * @return void
+     */
+    protected function validateValue(string $value): void
+    {
+        // Note: content must already be sanitized before validating
+        Assert::validLang($this->sanitizeValue($value), SchemaViolationException::class);
+    }
+}
