bc17d0130eb5c26d39c8cc0bb56e8a2d81e1e8ef
Tim van Dijen <tvdijen@gmail.com>
2025-01-11 16:57:20 +0100

    Create assertion and type-class for xs:NMTOKENS

---
+++ tests/Type/NMTokensValueTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XML\Type;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\Type\NMTokensValue;
+
+/**
+ * Class \SimpleSAML\Test\Type\NMTokensValueTest
+ *
+ * @package simplesamlphp/xml-common
+ */
+#[CoversClass(NMTokensValue::class)]
+final class NMTokensValueTest extends TestCase
+{
+    /**
+     * @param boolean $shouldPass
+     * @param string $nmtokens
+     */
+    #[DataProvider('provideNMTokens')]
+    public function testName(bool $shouldPass, string $nmtokens): void
+    {
+        try {
+            NMTokensValue::fromString($nmtokens);
+            $this->assertTrue($shouldPass);
+        } catch (SchemaViolationException $e) {
+            $this->assertFalse($shouldPass);
+        }
+    }
+
+
+    /**
+     * @return array<string, array{0: bool, 1: string}>
+     */
+    public static function provideNMTokens(): array
+    {
+        return [
+            'valid' => [true, 'Snoopy foobar'],
+            'diacritical' => [true, 'Snoopy fööbár'],
+            'start with colon' => [true, ':CMS :ABC'],
+            'start with dash' => [true, '-1950-10-04 -1984-11-07'],
+            'numeric first char' => [true, '0836217462'],
+            'comma' => [false, 'foo,bar'],
+            'whitespace collapse' => [true, "foobar\n"],
+            'normalization' => [true, ' foobar   nmtoken '],
+        ];
+    }
+}
