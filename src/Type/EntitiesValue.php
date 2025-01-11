a40a66495b51423f84f5f4f1bd9f11f14c5d3aae
Tim van Dijen <tvdijen@gmail.com>
2025-01-11 17:23:13 +0100

    Create assertion and type-class for xs:ENTITIES

---
+++ src/Type/EntitiesValue.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Type;
+
+use SimpleSAML\XML\Assert\Assert;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+/**
+ * @package simplesaml/xml-common
+ */
+class EntitiesValue extends TokenValue
+{
+    /**
+     * Validate the value.
+     *
+     * @param string $value
+     * @throws \SimpleSAML\XML\Exception\SchemaViolationException on failure
+     * @return void
+     */
+    protected function validateValue(string $value): void
+    {
+        // Note: value must already be sanitized before validating
+        Assert::validEntities($this->sanitizeValue($value), SchemaViolationException::class);
+    }
+}
---
+++ tests/Assert/EntitiesTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XML\Assert;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Assert\AssertionFailedException;
+use SimpleSAML\XML\Assert\Assert;
+
+/**
+ * Class \SimpleSAML\Test\XML\Assert\EntitiesTest
+ *
+ * @package simplesamlphp/xml-common
+ */
+#[CoversClass(Assert::class)]
+final class EntitiesTest extends TestCase
+{
+    /**
+     * @param boolean $shouldPass
+     * @param string $entities
+     */
+    #[DataProvider('provideEntities')]
+    public function testValidEntities(bool $shouldPass, string $entities): void
+    {
+        try {
+            Assert::validEntities($entities);
+            $this->assertTrue($shouldPass);
+        } catch (AssertionFailedException $e) {
+            $this->assertFalse($shouldPass);
+        }
+    }
+
+
+    /**
+     * @return array<int, array{0: bool, 1: string}>
+     */
+    public static function provideEntities(): array
+    {
+        return [
+            [true, 'Snoopy'],
+            [true, 'CMS'],
+            [true, 'fööbár'],
+            [false, '-1950-10-04'],
+            [false, '0836217462 0836217463'],
+            [true, 'foo bar'],
+            // Quotes are forbidden
+            [false, 'foo "bar" baz'],
+            // Commas are forbidden
+            [false, 'foo,bar'],
+            // Trailing newlines are forbidden
+            [false, "foobar\n"],
+        ];
+    }
+}
