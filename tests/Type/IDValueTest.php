710cac8fb5eaf5f90691e1d9937c9f714c8bdd42
Tim van Dijen <tvdijen@gmail.com>
2025-01-11 17:11:16 +0100

    Create assertion and type-class for xs:ID

---
+++ tests/Type/IDValueTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XML\Type;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\Type\IDValue;
+
+/**
+ * Class \SimpleSAML\Test\Type\IDValueTest
+ *
+ * @package simplesamlphp/xml-common
+ */
+#[CoversClass(IDValue::class)]
+final class IDValueTest extends TestCase
+{
+    /**
+     * @param boolean $shouldPass
+     * @param string $id
+     */
+    #[DataProvider('provideID')]
+    public function testID(bool $shouldPass, string $id): void
+    {
+        try {
+            IDValue::fromString($id);
+            $this->assertTrue($shouldPass);
+        } catch (SchemaViolationException $e) {
+            $this->assertFalse($shouldPass);
+        }
+    }
+
+
+    /**
+     * @return array<string, array{0: bool, 1: string}>
+     */
+    public static function provideID(): array
+    {
+        return [
+            'valid' => [true, 'Test'],
+            'valid starts with underscore' => [true, '_Test'],
+            'valid contains dashes' => [true, '_1950-10-04_10-00'],
+            'valid contains dots' => [true, 'Te.st'],
+            'valid contains diacriticals' => [true, 'fööbár'],
+            'valid prefixed v4 UUID' => [true, '_5425e58e-e799-4884-92cc-ca64ecede32f'],
+            'invalid empty string' => [false, ''],
+            'invalid contains wildcard' => [false, 'Te*st'],
+            'invalid starts with digit' => [false, '1Test'],
+            'invalid contains colon' => [false, 'Te:st'],
+            'whitespace collapse' => [true, "foobar\n"],
+            'normalization' => [true, ' foobar '],
+        ];
+    }
+}
