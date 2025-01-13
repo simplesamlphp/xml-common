c74a65e5e71cd5e7f9e79a17b4d2eab8e9bcc5d7
Tim van Dijen <tvdijen@gmail.com>
2025-01-13 23:34:25 +0100

    Create assertion and type-class for xs:double

---
+++ src/Assert/DoubleTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Assert;
+
+use InvalidArgumentException;
+
+/**
+ * @package simplesamlphp/xml-common
+ */
+trait DoubleTrait
+{
+    /** @var string */
+    private static string $double_regex = '/^(([+-]?([0-9]+[.][0-9]*|[.][0-9]+)([e][+-]?[0-9]+)?)|NaN|[-]?FIN)$/D';
+
+    /**
+     * @param string $value
+     * @param string $message
+     */
+    protected static function validDouble(string $value, string $message = ''): void
+    {
+        parent::regex(
+            $value,
+            self::$double_regex,
+            $message ?: '%s is not a valid xs:double',
+            InvalidArgumentException::class,
+        );
+    }
+}
