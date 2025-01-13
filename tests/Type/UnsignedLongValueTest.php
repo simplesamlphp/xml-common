5be5751c0df23d17889b10f12625a15a2683bd4f
Tim van Dijen <tvdijen@gmail.com>
2025-01-14 00:50:33 +0100

    Create assertion and type-class for xs:unsignedLong

---
+++ tests/Type/UnsignedLongValueTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XML\Type;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\Type\UnsignedLongValue;
+
+/**
+ * Class \SimpleSAML\Test\Type\UnsignedLongValueTest
+ *
+ * @package simplesamlphp/xml-common
+ */
+#[CoversClass(UnsignedLongValue::class)]
+final class UnsignedLongValueTest extends TestCase
+{
+    /**
+     * @param boolean $shouldPass
+     * @param string $unsignedLong
+     */
+    #[DataProvider('provideUnsignedLong')]
+    public function testUnsignedLong(bool $shouldPass, string $unsignedLong): void
+    {
+        try {
+            UnsignedLongValue::fromString($unsignedLong);
+            $this->assertTrue($shouldPass);
+        } catch (SchemaViolationException $e) {
+            $this->assertFalse($shouldPass);
+        }
+    }
+
+
+    /**
+     * @return array<string, array{0: bool, 1: string}>
+     */
+    public static function provideUnsignedLong(): array
+    {
+        return [
+            'empty' => [false, ''],
+            'valid positive integer' => [true, '18446744073709551615'],
+            'invalid positive out-of-bounds' => [false, '18446744073709551616'],
+            'valid signed positive integer' => [true, '+18446744073709551615'],
+            'valid zero' => [true, '0'],
+            'valid negative leading zeros' => [true, '0000000000000000000005'],
+            'invalid with fractional' => [false, '1.'],
+            'valid with whitespace collapse' => [true, " 1 234 \n"],
+            'invalid negative' => [false, '-1'],
+            'invalid with thousands-delimiter' => [false, '1,234'],
+        ];
+    }
+}
