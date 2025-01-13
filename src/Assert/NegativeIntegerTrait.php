508a436d4b1f32f63cf785dc1caaa599676f7cb8
Tim van Dijen <tvdijen@gmail.com>
2025-01-13 20:58:23 +0100

    Create assertion and type-class for xs:negativeInteger

---
+++ src/Assert/NegativeIntegerTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Assert;
+
+use InvalidArgumentException;
+
+/**
+ * @package simplesamlphp/xml-common
+ */
+trait NegativeIntegerTrait
+{
+    /** @var string */
+    private static string $negativeInteger_regex = '/^(-\d+)$/D';
+
+
+    /**
+     * @param string $value
+     * @param string $message
+     */
+    protected static function validNegativeInteger(string $value, string $message = ''): void
+    {
+        parent::regex(
+            $value,
+            self::$negativeInteger_regex,
+            $message ?: '%s is not a valid xs:negativeInteger',
+            InvalidArgumentException::class,
+        );
+    }
+}
