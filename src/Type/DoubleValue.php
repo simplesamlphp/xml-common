ee984efed49cff016ce41db421715a6ec380e59e
Tim van Dijen <tvdijen@gmail.com>
2025-01-13 23:34:25 +0100

    Create assertion and type-class for xs:double

---
+++ src/Type/DoubleValue.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Type;
+
+use SimpleSAML\XML\Assert\Assert;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+/**
+ * @package simplesaml/xml-common
+ */
+class DoubleValue extends AbstractValueType
+{
+    /**
+     * Sanitize the value.
+     *
+     * @param string $value  The unsanitized value
+     * @return string
+     */
+    protected function sanitizeValue(string $value): string
+    {
+        return str_replace(["\f", "\r", "\n", "\t", "\v", ' '], '', $value);
+    }
+
+
+    /**
+     * Validate the value.
+     *
+     * @param string $value
+     * @throws \SimpleSAML\XML\Exception\SchemaViolationException on failure
+     * @return void
+     */
+    protected function validateValue(string $value): void
+    {
+        Assert::validDouble($this->sanitizeValue($value), SchemaViolationException::class);
+    }
+}
