d75bc7e7c78af210b59315e8217a2b9437ffffca
Tim van Dijen <tvdijen@gmail.com>
2025-01-16 17:31:12 +0100

    Re-add element traits

--- tests/Utils/Base64BinaryElement.php
+++ tests/Utils/Base64BinaryElement.php
@@ -4,22 +4,18 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Test\XML;
 
-use DOMElement;
 use SimpleSAML\XML\AbstractElement;
-use SimpleSAML\XML\Assert\Assert;
-use SimpleSAML\XML\Exception\InvalidDOMElementException;
 use SimpleSAML\XML\{SchemaValidatableElementInterface, SchemaValidatableElementTrait};
-use SimpleSAML\XML\Type\Base64BinaryValue;
-
-use function strval;
+use SimpleSAML\XML\TypedTextContentTrait;
 
 /**
- * Empty shell class for testing Base64Binary elements.
+ * Empty shell class for testing xs:base64Binary elements.
  *
  * @package simplesaml/xml-common
  */
 final class Base64BinaryElement extends AbstractElement implements SchemaValidatableElementInterface
 {
+    use TypedTextContentTrait;
     use SchemaValidatableElementTrait;
 
     /** @var string */
@@ -31,44 +27,11 @@ final class Base64BinaryElement extends AbstractElement implements SchemaValidat
     /** @var string */
     public const SCHEMA = 'tests/resources/schemas/deliberately-wrong-file.xsd';
 
-
-    /**
-     * @param \SimpleSAML\XML\Type\Base64BinaryValue $content
-     */
-    public function __construct(
-        protected Base64BinaryValue $content,
-    ) {
-    }
+    /** @var string */
+    public const TEXTCONTENT_TYPE = AbstractElement::class; // Deliberately wrong class
 
 
     /**
-     * Create a class from XML
-     *
-     * @param \DOMElement $xml
-     * @return static
+     * NOTE: This class has some deliberately wrong values for testing purposes!!
      */
-    public static function fromXML(DOMElement $xml): static
-    {
-        Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
-        Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
-
-        $text = Base64BinaryValue::fromString($xml->textContent);
-
-        return new static($text);
-    }
-
-
-    /**
-     * Create XML from this class
-     *
-     * @param \DOMElement|null $parent
-     * @return \DOMElement
-     */
-    public function toXML(?DOMElement $parent = null): DOMElement
-    {
-        $e = $this->instantiateParentElement($parent);
-        $e->textContent = strval($this->content);
-
-        return $e;
-    }
 }
--- tests/Utils/StringElement.php
+++ tests/Utils/StringElement.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Test\XML;
 
-use DOMElement;
 use SimpleSAML\XML\AbstractElement;
-use SimpleSAML\XML\Assert\Assert;
-use SimpleSAML\XML\Exception\InvalidDOMElementException;
 use SimpleSAML\XML\{SchemaValidatableElementInterface, SchemaValidatableElementTrait};
+use SimpleSAML\XML\TypedTextContentTrait;
 use SimpleSAML\XML\Type\StringValue;
-
-use function strval;
 
 /**
  * Empty shell class for testing String elements.
@@ -21,6 +17,7 @@ use function strval;
 final class StringElement extends AbstractElement implements SchemaValidatableElementInterface
 {
     use SchemaValidatableElementTrait;
+    use TypedTextContentTrait;
 
     /** @var string */
     public const NS = 'urn:x-simplesamlphp:namespace';
@@ -31,44 +28,6 @@ final class StringElement extends AbstractElement implements SchemaValidatableEl
     /** @var string */
     public const SCHEMA = 'tests/resources/schemas/simplesamlphp.xsd';
 
-
-    /**
-     * @param \SimpleSAML\XML\Type\StringValue $content
-     */
-    public function __construct(
-        protected StringValue $content,
-    ) {
-    }
-
-
-    /**
-     * Create a class from XML
-     *
-     * @param \DOMElement $xml
-     * @return static
-     */
-    public static function fromXML(DOMElement $xml): static
-    {
-        Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
-        Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
-
-        $text = StringValue::fromString($xml->textContent);
-
-        return new static($text);
-    }
-
-
-    /**
-     * Create XML from this class
-     *
-     * @param \DOMElement|null $parent
-     * @return \DOMElement
-     */
-    public function toXML(?DOMElement $parent = null): DOMElement
-    {
-        $e = $this->instantiateParentElement($parent);
-        $e->textContent = strval($this->content);
-
-        return $e;
-    }
+    /** @var string */
+    public const TEXTCONTENT_TYPE = StringValue::class;
 }
