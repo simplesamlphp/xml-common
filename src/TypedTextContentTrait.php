e3d8613b071782b416c01e835fa97d69345686cd
Tim van Dijen <tvdijen@gmail.com>
2025-01-16 17:31:12 +0100

    Re-add element traits

---
+++ src/TypedTextContentTrait.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML;
+
+use DOMElement;
+use SimpleSAML\XML\Assert\Assert;
+use SimpleSAML\XML\Exception\{InvalidDOMElementException, InvalidValueTypeException};
+use SimpleSAML\XML\Type\{ValueTypeInterface, StringValue};
+
+use function defined;
+use function strval;
+
+/**
+ * Trait for elements that hold a typed textContent value.
+ *
+ * @package simplesaml/xml-common
+ */
+trait TypedTextContentTrait
+{
+    /**
+     * @param \SimpleSAML\XML\Type\ValueTypeInterface $content
+     */
+    public function __construct(
+        protected ValueTypeInterface $content,
+    ) {
+    }
+
+
+    /**
+     * Create a class from XML
+     *
+     * @param \DOMElement $xml
+     * @return static
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
+
+        $type = self::getTextContentType();
+        $text = $type::fromString($xml->textContent);
+
+        return new static($text);
+    }
+
+
+    /**
+     * Create XML from this class
+     *
+     * @param \DOMElement|null $parent
+     * @return \DOMElement
+     */
+    public function toXML(?DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+        $e->textContent = strval($this->getContent());
+
+        return $e;
+    }
+
+
+    /**
+     * Get the type the element's textContent value.
+     *
+     * @return class-string
+     */
+    public static function getTextContentType(): string
+    {
+        if (defined('static::TEXTCONTENT_TYPE')) {
+            $type = static::TEXTCONTENT_TYPE;
+        } else {
+            $type = StringValue::class;
+        }
+
+        Assert::isAOf($type, ValueTypeInterface::class, InvalidValueTypeException::class);
+        return $type;
+    }
+}
--- tests/Utils/BooleanElement.php
+++ tests/Utils/BooleanElement.php
@@ -4,23 +4,20 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Test\XML;
 
-use DOMElement;
 use SimpleSAML\XML\AbstractElement;
-use SimpleSAML\XML\Assert\Assert;
-use SimpleSAML\XML\Exception\InvalidDOMElementException;
 use SimpleSAML\XML\{SchemaValidatableElementInterface, SchemaValidatableElementTrait};
+use SimpleSAML\XML\TypedTextContentTrait;
 use SimpleSAML\XML\Type\BooleanValue;
 
-use function strval;
-
 /**
- * Empty shell class for testing String elements.
+ * Empty shell class for testing xs:string elements.
  *
  * @package simplesaml/xml-common
  */
 final class BooleanElement extends AbstractElement implements SchemaValidatableElementInterface
 {
     use SchemaValidatableElementTrait;
+    use TypedTextContentTrait;
 
     /** @var string */
     public const NS = 'urn:x-simplesamlphp:namespace';
@@ -31,44 +28,6 @@ final class BooleanElement extends AbstractElement implements SchemaValidatableE
     /** @var string */
     public const SCHEMA = 'tests/resources/schemas/simplesamlphp.xsd';
 
-
-    /**
-     * @param \SimpleSAML\XML\Type\BooleanValue $content
-     */
-    public function __construct(
-        protected BooleanValue $content,
-    ) {
-    }
-
-
-    /**
-     * Create a class from XML
-     *
-     * @param \DOMElement $xml
-     * @return static
-     */
-    public static function fromXML(DOMElement $xml): static
-    {
-        Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
-        Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
-
-        $text = BooleanValue::fromString($xml->textContent);
-
-        return new static($text);
-    }
-
-
-    /**
-     * Create XML from this class
-     *
-     * @param \DOMElement|null $parent
-     * @return \DOMElement
-     */
-    public function toXML(?DOMElement $parent = null): DOMElement
-    {
-        $e = $this->instantiateParentElement($parent);
-        $e->textContent = strval($this->content);
-
-        return $e;
-    }
+    /** @var string */
+    public const TEXTCONTENT_TYPE = BooleanValue::class;
 }
