d98324589b3508734045aebb2bf7dc77fddc7654
Tim van Dijen <tvdijen@gmail.com>
2025-02-04 11:45:03 +0100

    Add toInteger/fromInteger helpers

--- src/Type/IntegerValue.php
+++ src/Type/IntegerValue.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace SimpleSAML\XML\Type;
 
 use SimpleSAML\XML\Assert\Assert;
-use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\Exception\{RuntimeException, SchemaViolationException};
+
+use function bccomp;
+use function intval;
+use function strval;
 
 /**
  * @package simplesaml/xml-common
@@ -26,5 +30,34 @@ class IntegerValue extends DecimalValue
     protected function validateValue(string $value): void
     {
         Assert::validInteger($this->sanitizeValue($value), SchemaViolationException::class);
+    }
+
+
+    /**
+     * Convert from integer
+     *
+     * @param int $value
+     * @return static
+     */
+    public static function fromInteger(int $value): static
+    {
+        return new static(strval($value));
+    }
+
+
+    /**
+     * Convert to integer
+     *
+     * @return int
+     */
+    public function toInteger(): int
+    {
+        $value = $this->getValue();
+
+        if (bccomp($value, strval(PHP_INT_MAX)) === 1) {
+            throw new RuntimeException("Cannot convert to integer: out of bounds.");
+        }
+
+        return intval($value);
     }
 }
