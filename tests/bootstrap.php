9be4e534e1942c1ea88518a4cce71589acdca969
Tim van Dijen <tvdijen@gmail.com>
2025-06-28 23:59:54 +0200

    Bootstrap element registry for tests-suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,6 @@ declare(strict_types=1);
 
 $projectRoot = dirname(__DIR__);
 require_once($projectRoot . '/vendor/autoload.php');
+
+$registry = \SimpleSAML\XML\Registry\ElementRegistry::getInstance();
+$registry->importFromFile(dirname(__FILE__, 2) . '/classes/element.registry.php');
