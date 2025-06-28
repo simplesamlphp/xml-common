4222319c237ac5260b9d71facc51197b2716d4ec
Tim van Dijen <tvdijen@gmail.com>
2025-06-28 22:08:28 +0200

    Add type for xml:base attribute

---
+++ src/XML/Type/BaseValue.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Type;
+
+use SimpleSAML\XML\Attribute;
+use SimpleSAML\XML\Constants as C;
+use SimpleSAML\XMLSchema\Type\Builtin\AnyURIValue;
+use SimpleSAML\XMLSchema\Type\Helper\AttributeTypeInterface;
+
+/**
+ * @package simplesaml/xml-common
+ */
+class BaseValue extends AnyURIValue implements AttributeTypeInterface
+{
+    /** @var string */
+    public const SCHEMA_TYPE = 'xs:anyURI';
+
+
+    /**
+     * Convert this value to an attribute
+     *
+     * @return \SimpleSAML\XML\Attribute
+     */
+    public function toAttribute(): Attribute
+    {
+        return new Attribute(C::NS_XML, 'xml', 'base', $this);
+    }
+}
---
+++ tests/XML/Type/BaseValueTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XML\Type;
+
+use PHPUnit\Framework\Attributes\{CoversClass, DataProvider, DataProviderExternal, DependsOnClass};
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\XML\Assert\AnyURITest;
+use SimpleSAML\XML\Constants as C;
+use SimpleSAML\XML\Type\BaseValue;
+use SimpleSAML\XMLSchema\Exception\SchemaViolationException;
+use SimpleSAML\XMLSchema\Type\Builtin\{AnyURIValue, StringValue};
+
+/**
+ * Class \SimpleSAML\Test\XML\Type\BaseValueTest
+ *
+ * @package simplesamlphp/xml-common
+ */
+#[CoversClass(BaseValue::class)]
+final class BaseValueTest extends TestCase
+{
+    /**
+     * @param boolean $shouldPass
+     * @param string $base
+     */
+    #[DataProvider('provideValidBase')]
+    #[DataProviderExternal(AnyURITest::class, 'provideValidURI')]
+    #[DependsOnClass(AnyURITest::class)]
+    public function testAnyURI(bool $shouldPass, string $base): void
+    {
+        try {
+            BaseValue::fromString($base);
+            $this->assertTrue($shouldPass);
+        } catch (SchemaViolationException $e) {
+            $this->assertFalse($shouldPass);
+        }
+    }
+
+
+    /**
+     * Test helpers
+     */
+    public function testHelpers(): void
+    {
+        $base = BaseValue::fromString('urn:x-simplesamlphp:namespace');
+        $attr = $base->toAttribute();
+
+        $this->assertEquals($attr->getNamespaceURI(), C::NS_XML);
+        $this->assertEquals($attr->getNamespacePrefix(), 'xml');
+        $this->assertEquals($attr->getAttrName(), 'base');
+        $this->assertEquals($attr->getAttrValue(), 'urn:x-simplesamlphp:namespace');
+    }
+
+
+    /**
+     */
+    public function testEquals(): void
+    {
+        // Assert that two identical values are equal
+        $this->assertTrue(AnyURIValue::fromString('hello')->equals(AnyURIValue::fromString('hello')));
+        $this->assertTrue(AnyURIValue::fromString('hello')->equals(StringValue::fromString('hello')));
+        $this->assertTrue(AnyURIValue::fromString('hello')->equals('hello'));
+
+        // Assert that two different values are not equal
+        $this->assertFalse(AnyURIValue::fromString('hello')->equals(AnyURIValue::fromString('world')));
+        $this->assertFalse(AnyURIValue::fromString('hello')->equals(StringValue::fromString('world')));
+        $this->assertFalse(AnyURIValue::fromString('hello')->equals('world'));
+    }
+
+
+    /**
+     * @return array<string, array{0: true, 1: string}>
+     */
+    public static function provideValidBase(): array
+    {
+        return [
+            'trailing newline' => [true, "https://sts.windows.net/{tenantid}/\n"],
+        ];
+    }
+}
